6cd4874aec3414fb3cc409bcdf7cbc2bbd7868d8
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-14 11:35:32 +0000

    criado novos modelos

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app_admin')
 @section('content')
     <div class="section">
-        <h1>AngolaNews Admin</h1>
+        <h1>angoNews Admin</h1>
     </div>
     <div class="section">
         Seja bem vindo {{Auth::user()->pessoa->nome}}
