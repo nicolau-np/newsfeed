6309de549bd253900ee0f97ac5bf958019e12d00
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-14 16:47:25 +0000

    apagando comments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -243,7 +243,7 @@
             <ul class="nav nav-tabs" role="tablist">
               <li role="presentation" class="active"><a href="#category" aria-controls="home" role="tab" data-toggle="tab">Categorias</a></li>
               <li role="presentation"><a href="#video" aria-controls="profile" role="tab" data-toggle="tab">Video</a></li>
-              <li role="presentation"><a href="#comments" aria-controls="messages" role="tab" data-toggle="tab">Comentários</a></li>               
+              
             </ul>
             <div class="tab-content">
               <div role="tabpanel" class="tab-pane active" id="category">
@@ -258,50 +258,7 @@
                   <iframe width="100%" height="250" src="http://www.youtube.com/embed/h5QWbURNEpA?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
                 </div>
               </div>
-              <div role="tabpanel" class="tab-pane" id="comments">
-                <ul class="spost_nav">
-              <li>
-                <div class="media wow fadeInDown">
-                  <a href="single_page.html" class="media-left">
-                    <img alt="img" src="{{asset('assets/img/post_img1.jpg')}}">
-                  </a>
-                  <div class="media-body">
-                    <a href="single_page.html" class="catg_title"> Aliquam malesuada diam eget turpis varius 1</a>                        
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="media wow fadeInDown">
-                  <a href="single_page.html" class="media-left">
-                    <img alt="img" src="{{asset('assets/img/post_img2.jpg')}}">
-                  </a>
-                  <div class="media-body">
-                    <a href="single_page.html" class="catg_title"> Aliquam malesuada diam eget turpis varius 2</a>                        
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="media wow fadeInDown">
-                  <a href="single_page.html" class="media-left">
-                    <img alt="img" src="{{asset('assets/img/post_img1.jpg')}}">
-                  </a>
-                  <div class="media-body">
-                    <a href="single_page.html" class="catg_title"> Aliquam malesuada diam eget turpis varius 3</a>                        
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="media wow fadeInDown">
-                  <a href="single_page.html" class="media-left">
-                    <img alt="img" src="{{asset('assets/img/post_img2.jpg')}}">
-                  </a>
-                  <div class="media-body">
-                    <a href="single_page.html" class="catg_title"> Aliquam malesuada diam eget turpis varius 4</a>                       
-                  </div>
-                </div>
-              </li>
-            </ul>
-              </div>
+             
             </div>            
           </div>
           <!-- End tab section -->
