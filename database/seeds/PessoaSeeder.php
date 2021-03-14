65354ba2725b1009d9e80b1f81b7a684aa8e2239
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-14 10:37:13 +0000

    terminado seeder principais

--- database/seeds/PessoaSeeder.php
+++ database/seeds/PessoaSeeder.php
@@ -1,16 +1,28 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PessoaSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    static $pessoas = [
+        [
+            'id_municipio'=>1,
+            'nome'=>"Sebastiao Combe",
+            'genero'=>"M"
+        ],
+        [
+            'id_municipio'=>2,
+            'nome'=>"Fibras Admin",
+            'genero'=>"M"
+        ],
+    ];
     public function run()
     {
-        //
+        foreach(Self::$pessoas as $pessoa){
+            DB::table('pessoas')->insert(
+                $pessoa
+            );
+        }
     }
 }
