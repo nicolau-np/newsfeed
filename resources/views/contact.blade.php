d9ae166037cfc4e2fbb13beca331e1d73194ca25
Nicolau NP <mr1Normaliii@gmail.com>
2021-03-30 11:26:23 +0100

    mudando o sidebar noticias populares para include

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,3 @@
-<?php 
-use App\Http\Controllers\ControllerStatics;
-$pub_populares = ControllerStatics::getPubPopulares();
-?>
 @extends('layouts.app')
 @section('content')
      <!-- ==================start content body section=============== -->
@@ -26,23 +22,7 @@ $pub_populares = ControllerStatics::getPubPopulares();
          
           <div class="col-lg-4 col-md-4 col-sm-4">
             <aside class="right_content">
-              <div class="single_sidebar">
-                <h2><span>Publicações populares</span></h2>
-                <ul class="spost_nav">
-                  @foreach ($pub_populares as $pub_popular)
-                  <li>
-                    <div class="media wow fadeInDown">
-                    <a href="/single_page/{{$pub_popular->id}}" class="media-left">
-                        <img alt="{{$pub_popular->title}}" src="{{asset($pub_popular->imagem)}}">
-                      </a>
-                      <div class="media-body">
-                        <a href="/single_page/{{$pub_popular->id}}" class="catg_title">{{$pub_popular->title}}</a>                        
-                      </div>
-                    </div>
-                  </li>
-                  @endforeach
-                </ul>
-              </div>  
+             @include('includes.noticias_populares') 
             </aside>
           </div>
         </div>  
